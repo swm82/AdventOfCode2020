f1b7e51217268868e42d81513bc55a5b6c12ee70
swm82 <swm82@scarletmail.rutgers.edu>
2020-12-08 13:06:12 -0500

    Current version - DFS on original rule set

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -1,34 +1,49 @@
 import re
 
 class Graph:
-
     class Vertex:
-        def __init__(self):
+       def __init__(self):
             self.neighbors = []
 
     def __init__(self):
         self.vertices = {}
-def dfs(graph, v, final, visited, count):
-    visited[v] = True
+
+
+def dfs(graph, v, final, visited):
+    global count
+    print(f'\tVisiting: {v}')
+    # visited[v] = True
     if v == final:
-        return 1
+        count += 1
     for neighbor in graph[v]:
-        print(neighbor)
-        if neighbor not in visited:
-            if neighbor == final:
-                return 1
-            return dfs(graph, neighbor, final, visited, count)
+        if neighbor == final:
+            print(f'\t\tfound: {neighbor} ******************************')
+            count += 1
+        # if neighbor not in visited:
+        return dfs(graph, neighbor, final, visited)
     return 0
 
 
 if __name__ == '__main__':
+    global count
     count = 0
     filename = 'data.txt'
     graph = {}
     # Build graph using map of form {Vertex: [neighbor, .... neighbor]}
     with open(filename) as f:
-        graph = {y[0] : [a[2:] for a in re.split(', ', re.sub(' bag(s)*', '', y[1][:-1])) if not a[2:].startswith(' other')] for y in [x.split(' bags contain ') for x in f.read().rstrip().split('\n')]}
-    visited = {}
+        graph = {
+            y[0]: [
+                a[2:]
+                for a in re.split(', ', re.sub(' bag(s)*', '', y[1][:-1]))
+                if not a[2:].startswith(' other')
+            ]
+            for y in
+            [x.split(' bags contain ') for x in f.read().rstrip().split('\n')]
+        }
     for vertex in graph.keys():
-        count += dfs(graph, vertex, 'shiny gold', visited, count)
+        visited = {}
+        print(f'starting at: {vertex}')
+        dfs(graph, vertex, 'shiny gold', visited)
+    print(len(graph.keys()))
+    print(graph)
     print(count)
